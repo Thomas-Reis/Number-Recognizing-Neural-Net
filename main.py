0ebe634f0a989eb0cfaa3c8732fc73fb0264ca4f
Thomas Reis <chazz46@live.ca>
2019-03-20 16:54:34 -0400

    It fails to learn, gonna have to restart

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ y = tf.placeholder(dtype = tf.int32, shape = [None])
 images_flat = tf.contrib.layers.flatten(x)
 logits = tf.contrib.layers.fully_connected(images_flat, 10, tf.nn.relu)
 loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, logits = logits))
-train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+train_op = tf.train.AdamOptimizer(learning_rate=0.01).minimize(loss)
 correct_pred = tf.argmax(logits, 1)
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
@@ -35,7 +35,7 @@ sess = tf.Session()
 
 sess.run(tf.global_variables_initializer())
 
-for i in range(0, 50):
+for i in range(0, 25):
     for i in range(500):
             print('EPOCH', i)
             _, accuracy_val = sess.run([train_op, accuracy], feed_dict={x: train_images, y: train_labels})
@@ -46,31 +46,31 @@ for i in range(0, 50):
     import matplotlib.pyplot as plt
     import random
 
-    # Pick 10 random images
-    sample_indexes = random.sample(range(len(train_images)), 10)
-    sample_images = [train_images[i] for i in sample_indexes]
-    sample_labels = [train_labels[i] for i in sample_indexes]
+# Pick 10 random images
+sample_indexes = random.sample(range(len(train_images)), 10)
+sample_images = [train_images[i] for i in sample_indexes]
+sample_labels = [train_labels[i] for i in sample_indexes]
 
-    # Run the "correct_pred" operation
-    predicted = sess.run([correct_pred], feed_dict={x: sample_images})[0]
+# Run the "correct_pred" operation
+predicted = sess.run([correct_pred], feed_dict={x: sample_images})[0]
 
-    # Print the real and predicted labels
-    print(sample_labels)
-    print(predicted)
+# Print the real and predicted labels
+print(sample_labels)
+print(predicted)
 
-    # Display the predictions and the ground truth visually.
-    fig = plt.figure(figsize=(10, 10))
-    for i in range(len(sample_images)):
-        truth = sample_labels[i]
-        prediction = predicted[i]
-        plt.subplot(5, 2, 1 + i)
-        plt.axis('off')
-        color = 'green' if truth == prediction else 'red'
-        plt.text(5, 5, "Truth:        {0}\nPrediction: {1}".format(truth, prediction),
-                 fontsize=12, color=color)
-        plt.imshow(sample_images[i], cmap="gray")
+# Display the predictions and the ground truth visually.
+fig = plt.figure(figsize=(10, 10))
+for i in range(len(sample_images)):
+    truth = sample_labels[i]
+    prediction = predicted[i]
+    plt.subplot(5, 2, 1 + i)
+    plt.axis('off')
+    color = 'green' if truth == prediction else 'red'
+    plt.text(5, 5, "Truth:        {0}\nPrediction: {1}".format(truth, prediction),
+             fontsize=12, color=color)
+    plt.imshow(sample_images[i], cmap="gray")
 
-    plt.show()
+plt.show()
 
 print("break here")
 
