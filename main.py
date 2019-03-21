327afe78e7b4cae83584c88ee1efe09763e48c46
ChazzyDango <Thomas.Reis@uoit.net>
2019-03-20 21:46:22 -0400

    Added Fancy Plots

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ n_output = 10
 
 # Neural Network Variables
 learning_rate = 0.001
-n_iterations = 1000
-n_epochs = 10
+n_iterations = 100
+n_epochs = 200
 dropout = 0.5
 
 X = tf.placeholder("float", [None, n_input])
@@ -62,33 +62,31 @@ train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
 correct_pred = tf.equal(tf.argmax(output_layer, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
+
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
+
+loss = []
+loss_epoch = []
 for j in range(n_epochs):
-    print("Epoch " + str(j))
-    # train on mini batches
-    for i in range(n_iterations):
-        sess.run(train_step, feed_dict={X: train_images, Y: train_labels, keep_prob:dropout})
-        # print loss and accuracy (per minibatch)
-        if i%100==0:
-            minibatch_loss, minibatch_accuracy = sess.run([cross_entropy, accuracy], feed_dict={X: train_images, Y: train_labels, keep_prob:1.0})
-            print("Iteration", str(i), "\t| Loss =", str(minibatch_loss), "\t| Accuracy =", str(minibatch_accuracy))
+    # train on batch (all images)
+    sess.run(train_step, feed_dict={X: train_images, Y: train_labels, keep_prob:dropout})
+    # Gets the Loss and Accuracy on the set as of the current epoch
+    epoch_loss, epoch_accuracy = sess.run([cross_entropy, accuracy], feed_dict={X: train_images, Y: train_labels, keep_prob:1.0})
+    loss.append(epoch_loss)
+    loss_epoch.append(j)
+    print("Epoch ", str(j), "\t| Loss =", str(epoch_loss), "\t| Accuracy =", str(epoch_accuracy))
 
-test_accuracy = sess.run(accuracy, feed_dict={X: test_images, Y: test_labels, keep_prob: 1.0})
-print("\nAccuracy on test set:", test_accuracy)
+# Displays the Mean Squared Errors per Epoch
+plt.plot(loss_epoch, loss)
+plt.xlabel('Epochs')
+plt.ylabel("Mean Squared Error (Loss)")
+plt.title("Epochs vs Mean Squared Error")
+plt.show()
 
-rand_test = randint(0,9)
-prediction = sess.run(tf.argmax(output_layer,1), feed_dict={X: [test_images[rand_test]]})
-print ("Prediction for test image:", np.squeeze(prediction))
-print ("actual value for test image: " + str(rand_test))
-
-
-
-
-
-# Pick 10 random images
+# Pick 10 random images from the Test Set
 sample_indexes = random.sample(range(len(test_images)), 10)
 sample_images = [test_images[i] for i in sample_indexes]
 sample_labels = [test_labels[i] for i in sample_indexes]
@@ -96,23 +94,28 @@ sample_labels = [test_labels[i] for i in sample_indexes]
 # Run the "correct_pred" operation
 predicted = sess.run(tf.argmax(output_layer,1), feed_dict={X: sample_images})
 
-# Print the real and predicted labels
-# print(sample_labels)
-# print(predicted)
+
 
 # Display the predictions and the ground truth visually.
-fig = plt.figure(figsize=(10, 10))
+fig = plt.figure(figsize=(10, 5))
+
 for i in range(len(sample_images)):
     actual = np.where(sample_labels[i] == 1)
     prediction = predicted[i]
-    plt.subplot(5, 2, 1 + i)
+    # 5 rows, 2 columns, for 10 images. 1+i for the index of which subplot to write to (min of 1)
+    plt.subplot(3, 4, i + 1)
     plt.axis('off')
+    # sets the output color based on whether or not the Neural Net predicted correctly
     color = 'green' if actual == prediction else 'red'
+    # cleans the prediction for display
     predict_string = str(actual[0]).replace('[', '').replace(']', '')
-    plt.text(5, 5, "Actual:       {}\nPredicted: {}".format(predict_string, prediction),
-             fontsize=12, color=color)
+    plt.text(5, 5, "Actual = {}\nPredicted = {}".format(predict_string, prediction), color=color)
     plt.imshow(np.reshape(sample_images[i], (-1, 5)), cmap="gray")
 
+test_accuracy = sess.run(accuracy, feed_dict={X: sample_images, Y: sample_labels, keep_prob: 1.0})
+print("\nAccuracy on test set:", test_accuracy)
+
+fig.suptitle("Test Set Results", fontsize=36)
 plt.show()
 
 print("Complete!")
