860e2f18a56e779020f6db0c7e3478c1fa367430
ChazzyDango <Thomas.Reis@uoit.net>
2019-03-20 23:27:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -123,11 +123,14 @@ results = neural_network.run(tf.argmax(output_layer, 1), feed_dict={X: sample_im
 fig = plt.figure(figsize=(10, 5))
 
 num_correct = 0
+# iterates through results from sample set classification
 for i in range(len(sample_images)):
+    # sets the actual result as the INDEX of the output label (eg [0,0,1,0...0] its a result of 2
     actual_result = np.where(sample_labels[i] == 1)
     yielded_result = results[i]
-    # 5 rows, 2 columns, for 10 images. 1+i for the index of which subplot to write to (min of 1)
+    # 3 rows, 4 columns, for 10 images. 1+i for the index of which subplot to write to (min of 1)
     plt.subplot(3, 4, i + 1)
+    # disables the axis for the subplot for easier viewing
     plt.axis('off')
     # sets the output color based on whether or not the Neural Net predicted correctly
     if actual_result == yielded_result:
@@ -137,7 +140,9 @@ for i in range(len(sample_images)):
         color = 'red'
     # cleans the prediction for display
     predict_string = str(actual_result[0]).replace('[', '').replace(']', '')
+    # Displays text at 5,5 within the subplot (0,0 is the top left of the subplot)
     plt.text(5, 5, "Actual = {}\nPredicted = {}".format(predict_string, yielded_result), color=color)
+    # Adds the image to the plot
     plt.imshow(np.reshape(sample_images[i], (-1, 5)), cmap="gray")
 
 
